8024514ca73532d59bdacc26ec29a571d48788bf
mk-pmb <mk-pmb@users.noreply.github.com>
2016-12-18 00:48:55 +0100

    chain loader works

---
+++ test/fx.hello-world.js
@@ -0,0 +1,9 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+module.exports = {
+  wikifxInit: function (tags) {
+    tags.forEach(function (tag) { tag.innerHTML = 'Hello world!'; });
+  },
+};
