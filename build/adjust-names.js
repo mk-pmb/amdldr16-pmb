81b28d279209c04ec11e4038b4e3cb378bcc2fa9
mk-pmb <mk-pmb@users.noreply.github.com>
2016-12-18 20:41:28 +0100

    v0.1.6: chainload ./ as doc./

--- build/adjust-names.js
+++ build/adjust-names.js
@@ -1,27 +1,27 @@
 ﻿/*jslint indent: 2, maxlen: 80, browser: true */
 /* -*- tab-width: 2 -*- */
-/*globals define:true, curl:true */
+/*globals define:true */
 
 /* @license MIT | amdldr16-pmb: adjust names */
 (function () {
   'use strict';
-  function identity(x) { return x; }
-  define('curl/plugin/cjs', ['curl/loader/cjsm11'], identity);
+  function makeMemoFunc(x) { return function () { return x; }; }
 
-  (function (ld) {
-    // anon func hides ld from the outer scope, as to not clutter
-    // identity()'s closure with references to them.
-    ld = window[ld].noConflict();
-    window.lodash = ld;
-    define('lodash', function () { return ld; });
-  }('_'));
-
-  (function (jq) { // again, anon func to not taint identity
+  (function () {
+    var jq = '$', ld = '_';
     jq = window[jq].noConflict();
+
     window.jQuery = jq;
     // ^-- usually it's already there anyway.
-    define('jquery', function () { return jq; });
-  }('$'));
+    define('jquery', makeMemoFunc(jq));
+
+    ld = window[ld].noConflict();
+    window.lodash = ld;
+    define('lodash', makeMemoFunc(ld));
+
+    define('curl/plugin/cjs', ['curl/loader/cjsm11'], ld.identity);
+    define('window-pmb', window);
+  }());
 
 
 
