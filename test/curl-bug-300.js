425a8fb9421f7d937fd08f427dcf1ea79beac537
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-05 04:25:59 +0100

    v0.1.21: detect curl-amd bug #300

---
+++ test/curl-bug-300.js
@@ -0,0 +1,8 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var bug = { url: 'https://github.com/cujojs/curl/issues/300' };
+module.exports = bug;
+if (bug) { throw new Error("Unfortunately, you won't see this"); }
+bug.stillAlive = 'impossibru';
